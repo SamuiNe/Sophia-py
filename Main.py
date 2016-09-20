d28d72cf6ff9f4e99ee2808c9f67785a8b8fecd0
SamuiNe <gitgud@samuine.net>
2016-09-21 05:02:49 +0900

    Added ability to change command prefix

--- Main.py
+++ Main.py
@@ -54,6 +54,7 @@ async def on_message(message):
     if isperson:
         # TODO: Allow multiple combinations of servers for chat tunnelling
 
+        # Checks if the chat tunnelling mode is enabled and if the tunnelReceiveA and tunnelReceiveB is not none
         if tunnelEnable and tunnelReceiveA is not None and tunnelReceiveB is not None:
                 if tunnelReceiveA == message.channel:
                     await client.send_message(tunnelReceiveB, str(message.author) + 
@@ -62,16 +63,16 @@ async def on_message(message):
                     await client.send_message(tunnelReceiveA, str(message.author) + 
                         ' - ' + message.content)
 
-        if message.content.startswith('<'):
+        if message.content.startswith(prefixQualifier):
             if message.content.startswith(prefixQuestion):
-                if messagelow == '<?about':
+                if messagelow == prefixQuestion + 'about':
                     await client.send_message(message.channel, 'Hello! I am Sophia. Please treat me well!')
 
                 elif message.content == prefixQuestion + 'help' or message.content == prefixQuestion + 'commands':
                     await client.send_message(message.channel, "Here are the commands I recognize at the moment:\n\n" +
                         "*Question commands* (starts with `" + prefixQuestion + "`)\n" +
-                        "`about`, `messages`, `help` (`commands`), `invite`\n" +
-                        "*Information commands* (starts with `<!`)\n" +
+                        "`about`, `help` (`commands`), `invite`\n" +
+                        "*Information commands* (starts with `" + prefixInformation + "`)\n" +
                         "`hello`, `sara` (`sarachan`), `ping` (`pong`)\n" +
                         "*Trigger commands*\n" +
                         ":coffee:, :tea:, `cawfee`, `gween tea`, " +
@@ -83,7 +84,7 @@ async def on_message(message):
                     messlen = len(str(message.author))
                     await client.send_message(message.channel, 'Hello ' + str(message.author)[:messlen-5] + ' o/')
 
-                elif messagelow == '<!sarachan' or messagelow == '<!sara':
+                elif messagelow == prefixInformation + 'sarachan' or messagelow == prefixInformation + 'sara':
                     await client.send_message(message.channel, 'https://puu.sh/r5mt4/f9eb13dc29.gif')
 
                 # TODO: Add discord link to the discord server and continue to polish the bot
@@ -92,13 +93,13 @@ async def on_message(message):
                     # 'No permissions required!\n' +
                     # 'https://discordapp.com/oauth2/authorize?client_id=225810441610199040&scope=bot&permissions=0')
 
-                elif messagelow == '<!ping':
+                elif messagelow == prefixInformation + 'ping':
                     await client.send_message(message.channel, 'pong!')
 
-                elif messagelow == '<!pong':
+                elif messagelow == prefixInformation + 'pong':
                     await client.send_message(message.channel, 'ping!')
 
-                elif messagelow == '<!!debug':
+                elif messagelow == prefixDebug + 'debug':
                     # if message.author.id == ATSUI:
                     await client.send_message(message.channel, '`Author`: ' + str(message.author) +
                         ' `' + str(message.author.id) + '`\n' +
@@ -106,7 +107,7 @@ async def on_message(message):
                         '`Channel`: ' + str(message.channel) + ' `' + str(message.channel.id) + '`\n' +
                         '`Server`: ' + str(message.server.name) + ' `' + str(message.server.id) + '`')
 
-                elif messagelow.startswith('<!!tunnellink'):
+                elif messagelow.startswith(prefixDebug + 'tunnellink'):
                     if message.author.id == ATSUI:
                         global tunnelReceiveA
                         global tunnelReceiveB
@@ -126,7 +127,7 @@ async def on_message(message):
                             # await client.send_message(tunnelReceiveB, 'Test successful')
                             await client.send_message(message.channel, 'Channel B assignment successful')
 
-                elif messagelow.startswith('<!!tunnelenable'):
+                elif messagelow.startswith(prefixDebug + 'tunnelenable'):
                     if message.author.id == ATSUI:
                         tunnelenableparameter = str(messagelow)[16:]
                         await client.send_message(message.channel, tunnelenableparameter)
@@ -141,7 +142,7 @@ async def on_message(message):
                             tunnelEnable = False
                             await client.send_message(message.channel, 'Message tunneling disabled')
 
-                elif messagelow == '<!!tunnelinfo':
+                elif messagelow == prefixDebug + 'tunnelinfo':
                     if message.author.id == ATSUI:
                         if tunnelReceiveA is not None:
                             tunnelida = tunnelReceiveA.id
@@ -159,22 +160,112 @@ async def on_message(message):
                             ' `' + tunnelidb + '`' +
                             '\n' + '`Tunnel Boolean`: ' + str(tunnelEnable))
 
-                elif messagelow == '<!!prefixchange iknowwhatwouldhappen':
+                elif messagelow.startswith(prefixDebug + 'prefixchange'):
                     if message.author.id == ATSUI:
-                        await client.send_message(message.channel, 'Prefix change failed since Atsui is lazy')
+                        tempcollection = ['<', None, None, None]
+                        exceptioncheck = False
+                        findqualifier = ' '
+                        findcheckbefore = 0
+                        findcheckafter = 0
+                        findcount = 0
+                        processcount = 2
+                        tempcounter = 0
+                        exceptioncounter = 0
+                        processedprefix = [0, None, None, None, None, None]
 
-                elif messagelow == '<!!suspend':
+                        while findcheckafter != -1 and findcount != 5:
+                            findcheckbefore = findcheckafter
+                            findcheckafter = messagelow.find(findqualifier, findcheckbefore)
+
+                            if findcheckafter != -1:
+                                findcount += 1
+                                processedprefix[findcount] = findcheckafter
+
+                        while processedprefix[processcount] is not None and processcount != 6:
+                            tempcollection[tempcounter] = messagelow[processedprefix[processcount - 1] + 1:
+                                (processedprefix[processcount] - processedprefix[processcount - 1]) - 2]
+                            tempcounter += 1
+                            processcount += 1
+                        ''' Old Code
+                        if processedprefix[2] is not None:
+                            tempqualifier = messagelow[processedprefix[1] + 1: \
+                                (processedprefix[2] - processedprefix[1]) - 2]
+
+                        if processedprefix[3] is not None:
+                            tempquestion = messagelow[processedprefix[2] + 1: \
+                                (processedprefix[3] - processedprefix[2]) - 2]
+
+                        if processedprefix[4] is not None:
+                            tempinformation = messagelow[processedprefix[3] + 1: \
+                                (processedprefix[4] - processedprefix[3]) - 2]
+
+                        if processedprefix[5] is not None:
+                            tempdebug = messagelow[processedprefix[4] + 1: \
+                                (processedprefix[5] - processedprefix[4]) - 2]
+                        '''
+
+                        if tempcollection[exceptioncounter] is not None and \
+                                tempcollection[exceptioncounter + 1] is not None:
+
+                            while tempcollection[exceptioncounter] is not None and \
+                                    [exceptioncounter + 1] is not None and exceptioncounter != 4:
+
+                                exceptioncounter += 1
+                                if not tempcollection[exceptioncounter].startswith(tempcollection[0]):
+                                    exceptioncheck = True
+                        ''' Old code
+                        if tempqualifier is not None and tempquestion is not None:
+                            if tempquestion is not None and not tempquestion.startswith(tempqualifier):
+                                exceptioncheck = True
+
+                            if tempinformation is not None and not tempinformation.startswith(tempqualifier):
+                                exceptioncheck = True
+
+                            if tempdebug is not None and not tempinformation.startswith(tempqualifier):
+                                exceptioncheck = True
+                        '''
+
+                        if exceptioncheck:
+                            await client.send_message(message.channel, 'Prefix change failed.')
+                            await client.send_message(message.channel, 'Debug information:\n' + str(findcheckbefore) +
+                                ' ' + str(findcheckafter) + ' ' + str(findcount) + ' ' + str(processcount) + ' ' +
+                                str(tempcounter) + ' ' + str(exceptioncounter) + '\n' +
+                                str(exceptioncheck) + '\n' +
+                                '>' + processedprefix[0] + ' ' + processedprefix[1] + ' ' + processedprefix[2] + ' ' +
+                                processedprefix[3] + ' ' + processedprefix[4] + ' ' + processedprefix[5] + '\n' +
+                                '>>' + tempcollection[0] + ' ' + tempcollection[1] + ' ' + tempcollection[2] + ' ' +
+                                tempcollection[3])
+                        else:
+                            global prefixQualifier
+                            global prefixQuestion
+                            global prefixInformation
+                            global prefixDebug
+                            prefixQualifier = tempcollection[0]
+                            prefixQuestion = tempcollection[1]
+                            prefixInformation = tempcollection[2]
+                            prefixDebug = tempcollection[3]
+                            await client.send_message(message.channel, 'Prefix change success.')
+                            await client.send_message(message.channel, 'Debug information:\n' + str(findcheckbefore) +
+                                ' ' + str(findcheckafter) + ' ' + str(findcount) + ' ' + str(processcount) + ' ' +
+                                str(tempcounter) + ' ' + str(exceptioncounter) + '\n' +
+                                str(exceptioncheck) + '\n' +
+                                '>' + processedprefix[0] + ' ' + processedprefix[1] + ' ' + processedprefix[2] + ' ' +
+                                processedprefix[3] + ' ' + processedprefix[4] + ' ' + processedprefix[5] + '\n' +
+                                '>>' + tempcollection[0] + ' ' + tempcollection[1] + ' ' + tempcollection[2] + ' ' +
+                                tempcollection[3])
+
+                elif messagelow == prefixDebug + 'suspend':
                     if message.author.id == ATSUI:
                         await asyncio.sleep(5)
                         await client.send_message(message.channel, 'Suspend complete')
 
-                elif message.content.startswith('<!!playchange'):
+                elif message.content.startswith(prefixDebug + 'playchange'):
                     if message.author.id == ATSUI:
                         gamemessage = str(message.content)[14:]
                         await client.change_status(game=discord.Game(name=gamemessage), idle=False)
                         await client.send_message(message.channel, 'Playing message successfully updated')
 
-                elif message.content.startswith('<!!testingmode'):
+                elif message.content.startswith(prefixDebug + 'testingmode'):
                     if message.author.id == ATSUI:
                         testingmodeparameter = str(messagelow)[15:]
                         await client.send_message(message.channel, testingmodeparameter)
@@ -189,19 +280,19 @@ async def on_message(message):
                             testingMode = False
                             await client.send_message(message.channel, 'Testing mode disabled')
 
-                elif messagelow == '<!!rest':
+                elif messagelow == prefixDebug + 'rest':
                     if message.author.id == ATSUI:
                         await client.send_message(message.channel, 'I will rest for now. Good night!')
                         await client.logout()
 
-                elif messagelow == '<!!whack':
+                elif messagelow == prefixDebug + 'whack':
                     if message.author.id == ATSUI:
                         await client.send_message(message.channel, 'o-ow!')
                         await asyncio.sleep(5)
                         await client.send_message(message.channel, 'zzz')
                         await client.logout()
 
-                elif messagelow == '<!!selfdestruct':
+                elif messagelow == prefixDebug + 'selfdestruct':
                     if message.author.id == ATSUI:
                         await client.send_message(message.channel, ':boom:')
                         await client.logout()
