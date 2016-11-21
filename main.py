d4cc08d6be6be4d9990c58c868613a5bfa9ea68f
SamuiNe <gitgud@samuine.net>
2016-11-21 16:12:05 +0900

    Simplified main.py
    Version 0.2.7 > 0.2.8
    fragmented main.py to 4 different files
    added commands_debug.py
    added commands_information.py
    added commands_question.py

--- main.py
+++ main.py
@@ -3,16 +3,15 @@
 
 # python built-in modules
 import logging
-import traceback
-import random
 import sys
 
 # other modules
 import asyncio
 import discord
-import psutil
 
 # sophia modules
+import commands_information
+import commands_question
 import bot_system
 import room
 import chat_tunnel
@@ -56,7 +55,7 @@ RoomInfo = room.RoomInformations([], [['Blackjack'], [6]], ('Waiting', 'In Progr
         [['Testing room', 'Blackjack', 'Testing', '0']])
 TunnelInfo = chat_tunnel.TunnelInformations([], [], [], [], [], [[[True, False, 'Global Chat', '']]])
 sophia = discord.Client()
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 
 
 @sophia.event
@@ -110,151 +109,12 @@ async def on_message(message):
 
         if message.content.startswith(System.prefix_qualifier):
             if message.content.startswith(System.prefix_question):
-                if message_low == System.prefix_question + 'about':
-                    await sophia.send_message(message.channel, 'Hello! I am Sophia. Please treat me well!')
-
-                elif message_low == System.prefix_question + 'botversion':
-                    await sophia.send_message(message.channel, 'My current version is ' + __version__ +
-                        ', which is last updated at 2016/11/12.')
-
-                elif message.content == System.prefix_question + 'help':
-                    await bot_system.command_help(System, sophia, message)
-
-                elif message_low.startswith(System.prefix_question + 'command'):
-                    await bot_system.individual_command_help(System, sophia, message)
-
-                elif message_low == System.prefix_question + 'infocheck':
-                    await bot_system.info_check(sophia, message)
-
-                elif message_low.startswith(System.prefix_question + 'tunnelcheck'):
-                    await chat_tunnel.tunnel_information(sophia, message, TunnelInfo)
+                await commands_question.question_process(bot_system, chat_tunnel, System, sophia, message, message_low,
+                    TunnelInfo, __version__)
 
             elif message.content.startswith(System.prefix_information):
-                if message_low == System.prefix_information + 'ping':
-                    ping_message = 'pong!'
-                    await bot_system.detailed_ping(System, sophia, message, ping_message)
-
-                elif message_low == System.prefix_information + 'pong':
-                    ping_message = 'ping!'
-                    await bot_system.detailed_ping(System, sophia, message, ping_message)
-
-                elif message.content == System.prefix_information + 'hello':
-                    mess_len = len(str(message.author))
-                    await sophia.send_message(message.channel, 'Hello ' + str(message.author)[:mess_len - 5] + ' o/')
-
-                elif message_low == System.prefix_information + 'sarachan' or \
-                        message_low == System.prefix_information + 'sara':
-                    await sophia.send_message(message.channel, 'http://sophia.samuine.net/content/scratchwalls.gif')
-
-                elif message_low == System.prefix_information + 'invite':
-                    await bot_system.server_invite(sophia, message)
-
-                elif message_low.startswith(System.prefix_information + 'tunnel'):
-                    if message_low.startswith(System.prefix_information + 'tunnellink'):
-                        await chat_tunnel.tunnel_link(System, discord, sophia, message, TunnelInfo)
-
-                    elif message_low.startswith(System.prefix_information + 'tunnelenable'):
-                        await chat_tunnel.tunnel_enable(System, sophia, message, message_low, TunnelInfo)
-
-                    elif message_low.startswith(System.prefix_information + 'tunnelmode'):
-                        await chat_tunnel.tunnel_mode(System, sophia, message, TunnelInfo)
-
-                    elif message_low.startswith(System.prefix_information + 'tunnelcreate'):
-                        await chat_tunnel.tunnel_create(discord, System, sophia, message, TunnelInfo)
-
-                    elif message_low.startswith(System.prefix_information + 'tunnelleave'):
-                        await chat_tunnel.tunnel_leave(System, sophia, message, TunnelInfo)
-
-                    elif message_low.startswith(System.prefix_information + 'tunneldelete'):
-                        await chat_tunnel.tunnel_delete(asyncio, System, sophia, message, TunnelInfo)
-
-                elif message_low.startswith(System.prefix_information + 'triggertoggle'):
-                    permission_check = await chat_tunnel.permission_check(System, message)
-                    await bot_system.trigger_toggle(System, sophia, message, message_low, permission_check)
-
-                elif message.author.id in System.ATSUI:
-                    if message_low.startswith(System.prefix_debug + 'eval'):
-                        message_qualifier = ' '
-                        message_index = message.content.find(message_qualifier, 0)
-                        message_split = message.content[message_index + 1:]
-
-                        if message_split != '' and message_index != -1:
-                            if message_split not in System.forbidden_eval:
-                                try:
-                                    message_send = eval(message_split)
-                                except BaseException:
-                                    await sophia.send_message(message.channel,
-                                        System.eval_error_message[random.randrange(
-                                                System.eval_error_length)] + '\n' +
-                                        '```py\n' + traceback.format_exc() + '```')
-                                else:
-                                    await sophia.send_message(message.channel, message_send)
-                            else:
-                                await sophia.send_message(message.channel, 'nope')
-                        else:
-                            await sophia.send_message(message.channel, ':eyes:')
-
-                    elif message_low.startswith(System.prefix_debug + 'roomcheck'):
-                        await room.room_check(RoomInfo, sophia, message, message_low)
-
-                    elif message_low.startswith(System.prefix_debug + 'roomcreate'):
-                        await room.room_create(System, RoomInfo, sophia, message, message_low)
-
-                    elif message_low.startswith(System.prefix_debug + 'roomjoin'):
-                        await room.room_join(RoomInfo, sophia, message, message_low)
-
-                    elif message_low == System.prefix_debug + 'secret':
-                        await sophia.send_message(message.channel, 'Nothing to see here!')
-
-                    elif message_low.startswith(System.prefix_debug + 'prefixchange'):
-                        await bot_system.prefix_change(System, sophia, message, message_low)
-
-                    elif message_low == System.prefix_debug + 'mpstatus':
-                        await sophia.send_message(message.channel, 'Current MP status:\n\n' +
-                            '`Allocated` '+ str(round(((sys.getallocatedblocks() * 512) / 1024 ** 2), 2)) +
-                            ' (py) / ' +
-                            str(round(psutil.virtual_memory().used / (1024 ** 2), 2)) + ' `MP` (' +
-                            str(psutil.virtual_memory().percent) + '%)\n' +
-                            '`Available` ' + str(round(psutil.virtual_memory().available / (1024 ** 2), 2)) +
-                            ' / ' + str(round(psutil.virtual_memory().total / (1024 ** 2), 2)) + ' `MP`')
-
-                    elif message_low == System.prefix_debug + 'suspend':
-                        await asyncio.sleep(5)
-                        await sophia.send_message(message.channel, 'Suspend complete')
-
-                    elif message.content.startswith(System.prefix_debug + 'playchange'):
-                        game_message = str(message.content)[14:]
-                        System.previous_playing_message = game_message
-
-                        await sophia.change_presence(game=discord.Game(name=game_message))
-                        await sophia.send_message(message.channel, 'Playing message has successfully updated.')
-
-                    elif message.content.startswith(System.prefix_debug + 'testmode'):
-                        await bot_system.testing_mode(System, discord, sophia, message, message_low)
-
-                    elif message_low == System.prefix_debug + 'rest':
-                        await sophia.send_message(message.channel, 'I will rest for now. Good night!')
-                        await sophia.logout()
-
-                    elif message_low == System.prefix_debug + 'whack':
-                        await sophia.send_message(message.channel, 'o-ow!')
-                        await asyncio.sleep(5)
-                        await sophia.send_message(message.channel, 'zzz')
-                        await sophia.logout()
-
-                    elif message_low == System.prefix_debug + 'explosion':
-                        await sophia.send_message(message.channel, 'explooooosion!')
-                        await asyncio.sleep(0.5)
-                        await sophia.send_message(message.channel, ':boom:')
-                        await asyncio.sleep(4.2)
-                        await sophia.send_message(message.channel, 'zzz')
-                        await sophia.logout()
-
-                    elif message_low.startswith(System.prefix_debug + 'changename'):
-                        await bot_system.change_name(sophia, message)
-
-                    elif message_low.startswith(System.prefix_debug + 'changeavatar'):
-                        await bot_system.change_avatar(sophia, message)
+                await commands_information.information_process(sys, asyncio, discord, bot_system, room, chat_tunnel,
+                    System, sophia, message, message_low, TunnelInfo, RoomInfo)
 
         else:
             if message.server.id in System.trigger_include:
@@ -300,8 +160,6 @@ async def on_message(message):
                             while loop_count != loop_max and send_allowed:
                                 if TunnelInfo.tunnel_receive[int(TunnelInfo.channel_relation[channel_point][2])][
                                         loop_count][1] >= 2:
-                                    # if :
-                                    #   loop_count = TunnelInfo.channel_relation[channel_point][1]
                                     if loop_count != TunnelInfo.channel_relation[channel_point][1]:
                                         await sophia.send_message(TunnelInfo.tunnel_receive[
                                                 int(TunnelInfo.channel_relation[channel_point][2])][loop_count][0],
